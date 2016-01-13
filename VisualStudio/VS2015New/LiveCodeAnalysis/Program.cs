e5608b7b2b5348ea13245f8a0c30f3a91a090f3a
Tomasz Wisniewski <tomasz.wisniewski@windowslive.com>
2016-01-13 16:03:30 +0100

    Added live code analysis sample

---
+++ VisualStudio/VS2015New/LiveCodeAnalysis/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure;
+using System;
+
+namespace LiveCodeAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            //Create the blob client object.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            //Get a reference to a container to use for the sample code, and create it if it does not exist.
+            CloudBlobContainer container = blobClient.GetContainerReference("sascontainer");
+            container.CreateIfNotExists();
+
+            //Insert calls to the methods created below here...
+
+            //Generate a SAS URI for the container, without a stored access policy.
+            Console.WriteLine("Container SAS URI: " + GetContainerSasUri(container));
+            Console.WriteLine();
+
+            //Require user input before closing the console window.
+            Console.ReadLine();
+        }
+
+        static string GetContainerSasUri(CloudBlobContainer container)
+        {
+            //Set the expiry time and permissions for the container.
+            //In this case no start time is specified, so the shared access signature becomes valid immediately.
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24);
+            sasConstraints.SharedAccessStartTime = DateTime.UtcNow;
+            sasConstraints.Permissions = SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.List;
+
+            //Generate the shared access signature on the container, setting the constraints directly on the signature.
+            string sasContainerToken = container.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
+        }
+    }
+}
