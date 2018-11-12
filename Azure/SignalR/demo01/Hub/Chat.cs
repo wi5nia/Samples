39e31331274cae471f6429149bfd3e19e0f413c8
Tomasz Wisniewski <wisnia@windowslive.com>
2018-11-12 12:10:53 +0100

    Local SignalR version

---
+++ Azure/SignalR/demo01/Hub/Chat.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace demo01
+{
+    public class Chat : Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+    }
+}
